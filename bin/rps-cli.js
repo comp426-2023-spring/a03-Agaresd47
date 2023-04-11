2ea1edbec248367f4aee899c143fc7968103b43d
Agaresd47 <zdong@unc.edu>
2023-04-11 15:21:37 -0400

    cli

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,1 +1,50 @@
 #!/usr/bin/env node
+
+import minimist from 'minimist'
+import { rps } from "/lib/rpsls.js"
+
+var argv = minimist(process.argv.slice(2))
+
+function printHelp() {
+    console.log(`Usage: node-rpsls [SHOT]
+    Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+    
+      -h, --help        display this help message and exit
+      -r, --rules       display the rules and exit
+    
+    Examples:
+      node-rpsls        Return JSON with single player RPSLS result.
+                        e.g. {"player":"rock"}
+      node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                        e.g {"player":"rock","opponent":"Spock","result":"lose"}`)
+}
+
+function printRules() {
+    console.log(`Rules for Rock Paper Scissors:
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock CRUSHES Scissors`)
+}
+
+
+// Help
+if (argv.h !== undefined || argv.help !== undefined) {
+    printHelp()
+    process.exit(0)
+}
+
+// Rules
+if (argv.r !== undefined || argv.rules !== undefined) {
+    printRules()
+    process.exit(0)
+}
+
+try {
+    let result = rps((argv._)[0])
+    console.log(JSON.stringify(result))
+    process.exit(0)
+} catch (e) {
+    printHelp()
+    printRules()
+    process.exit(1)
+}
